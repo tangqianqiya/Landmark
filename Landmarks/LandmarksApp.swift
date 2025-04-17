6d6c9e54ba0c4777a7a779606f08e90fca060f9c
Tang Qianqi <tangqianqiya@gmail.com>
2025-04-17 13:17:17 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Tang Qianqi on 2025/4/17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
